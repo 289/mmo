d1183373413dbc5f624d7f286a34a7787cd20fd2
jinshuai <289070379@qq.com>
2016-09-04 02:56:02 +0800

    123

--- src/main/java/mmo/db/role/entity/Role.java
+++ src/main/java/mmo/db/role/entity/Role.java
@@ -24,160 +24,111 @@ public class Role extends DbEntity {
     }
 
     public Integer getId() {
-        return id;
+        return (Integer) commonGet(this, 1);
     }
 
     public void setId(final Integer id) {
         if (this.id != null && this.id.equals(id)) {
             return;
         }
-        if (isMirror) {
-            this.id = id;
-            return;
-        }
-        Transaction transaction = Transaction.current();
-        if (transaction != null) {
-            final Integer curr = this.id;
-            transaction.addTRecord(new TransLog() {
-                @Override
-                public void commit() {
-                    mirrorLock.lock();
-                    try {
-                        Role role = getMirror();
-                        role.setId(id);
-                        role.onUpdate();
-                    } finally {
-                        mirrorLock.unlock();
-                    }
-                }
-
-                @Override
-                public void rollback() {
-                    Role.this.id = curr;
-                }
-            });
-        } else {
-            mirrorLock.lock();
-            try {
-                Role role = getMirror();
-                role.setId(id);
-                role.onUpdate();
-            } finally {
-                mirrorLock.unlock();
-            }
+        if (!isMirror) {
+            transactionHandle(1, this.nickname, nickname);
         }
         this.id = id;
     }
 
     public String getNickname() {
-        return nickname;
+        return (String) commonGet(this, 2);
     }
 
     public void setNickname(final String nickname) {
         if (this.nickname != null && this.nickname.equals(nickname)) {
             return;
         }
-        if (isMirror) {
-            this.nickname = nickname == null ? null : nickname.trim();
-            return;
-        }
-        Transaction transaction = Transaction.current();
-        if (transaction != null) {
-            final String curr = this.nickname;
-            transaction.addTRecord(new TransLog() {
-                @Override
-                public void commit() {
-                    mirrorLock.lock();
-                    try {
-                        Role role = getMirror();
-                        role.nickname = nickname;
-                        role.onUpdate();
-                    } finally {
-                        mirrorLock.unlock();
-                    }
-                }
-
-                @Override
-                public void rollback() {
-                    Role.this.nickname = curr;
-                }
-            });
-        } else {
-            mirrorLock.lock();
-            try {
-                Role role = getMirror();
-                role.setNickname(nickname);
-                role.onUpdate();
-            } finally {
-                mirrorLock.unlock();
-            }
+        if (!isMirror) {
+            transactionHandle(2, this.nickname, nickname);
         }
         this.nickname = nickname == null ? null : nickname.trim();
     }
 
     public Integer getLevel() {
-        return level;
+        return (Integer) commonGet(this, 3);
     }
 
     public void setLevel(final Integer level) {
         if (this.level != null && this.level.equals(level)) {
             return;
         }
-        if (isMirror) {
-            this.level = level;
-            return;
-        }
-        Transaction transaction = Transaction.current();
-        if (transaction != null) {
-            final Integer curr = this.level;
-            transaction.addTRecord(new TransLog() {
-                @Override
-                public void commit() {
-                    mirrorLock.lock();
-                    try {
-                        Role role = getMirror();
-                        role.setLevel(level);
-                        role.onUpdate();
-                    } finally {
-                        mirrorLock.unlock();
-                    }
-                }
-
-                @Override
-                public void rollback() {
-                    Role.this.level = curr;
-                }
-            });
-        } else {
-            mirrorLock.lock();
-            try {
-                Role role = getMirror();
-                role.setLevel(level);
-                role.onUpdate();
-            } finally {
-                mirrorLock.unlock();
-            }
+        if (!isMirror) {
+            transactionHandle(3, this.level, level);
         }
         this.level = level;
     }
 
+    public void transactionHandle(final int index, Object old, final Object newVal){
+        Transaction transaction = Transaction.current();
+        if (transaction != null) {
+            final Object curr = old;
+            transaction.addTRecord(new TransLog() {
+                @Override
+                public void commit() {
+                    updateToMirror(index, newVal);
+                }
+
+                @Override
+                public void rollback() {
+                    commonSet(Role.this, index, curr);
+                }
+            });
+        } else {
+            updateToMirror(index, newVal);
+        }
+    }
+
+    public void updateToMirror(int index, Object val){
+        while (state.get() != WRITE_THREAD){
+            //spin
+            state.compareAndSet(NONE_THREAD, UPDATE_THREAD);
+        }
+        Role mirror = getMirror();
+        commonSet(mirror, index, val);
+        mirror.onUpdate();
+        state.set(NONE_THREAD);
+    }
+
+    public Role getMirrorThenClear(){
+        Role mirror = null;
+        if(mirrorEntity != null && state.compareAndSet(NONE_THREAD, UPDATE_THREAD)){
+            mirror = mirrorEntity.cast();
+            mirrorEntity = null;
+            state.set(NONE_THREAD);
+        }
+        return mirror;
+    }
+
+    public void commonSet(Role role, int index, Object val){
+        switch (index){
+            case 1 : break;
+            case 2 : role.setLevel((Integer) val);break;
+            case 3 : role.setNickname((String) val);break;
+        }
+    }
+
+    public Object commonGet(Role role, int index){
+        switch (index){
+            case 1 : return role.id;
+            case 2 : return role.nickname;
+            case 3 : return role.level;
+        }
+        throw new RuntimeException();
+    }
+
     @Override
     public void update() {
-        if (mirrorEntity != null && mirrorLock.tryLock()) {
-            Role mirror = null;
-            try {
-                if (mirrorEntity != null ) {
-                    mirror = mirrorEntity.cast();
-                    mirrorEntity = null;
-                }
-            } finally {
-                mirrorLock.unlock();
-            }
-            if(mirror != null && mirror.mark == UPDATE){
-                MapperMgr.getMapper(RoleMapper.class).updateByPrimaryKeySelective(mirror);
-            }
+        Role mirror = getMirrorThenClear();
+        if(mirror != null && mirror.mark == UPDATE){
+            MapperMgr.getRoleMapper(RoleMapper.class).updateByPrimaryKeySelective(mirror);
         }
-
     }
 
     @Override
